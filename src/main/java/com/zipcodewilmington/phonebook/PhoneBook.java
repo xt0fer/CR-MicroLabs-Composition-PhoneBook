cfa24a71b108b8cae5fa9b7893414b08dead8af0
xt0fer <kryounger@gmail.com>
2018-02-12 17:44:21 -0500

    phase I complete.

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,49 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.*;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private Map<String, String> phonebook;
+
+    public PhoneBook() {
+        this.phonebook = new HashMap<String, String>();
+    }
+
+    public void add(String name, String number) {
+        phonebook.put(name,number);
+    }
+
+    public String lookup(String name) {
+        return phonebook.get(name);
+    }
+
+    public void remove(String name, String number) {
+        phonebook.remove(name);
+    }
+
+    public String listNamesAndNumbers() {
+        Map<String, String> sortedPhoneBook = new TreeMap<String, String>(phonebook);
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String> entry : sortedPhoneBook.entrySet()) {
+            sb.append(entry.getKey());
+            sb.append(" ");
+            sb.append(entry.getValue());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public String listNames() {
+        Map<String, String> sortedPhoneBook = new TreeMap<String, String>(phonebook);
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String> entry : sortedPhoneBook.entrySet()) {
+            sb.append(entry.getKey());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,65 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
 /**
- * Created by leon on 1/23/18.
+ * Created by kristofer on 2/12/18.
  */
 public class PhoneBookTest {
+
+    /**
+     * Created by dan on 6/14/17.
+     */
+
+    @Test
+    public void testAddToPhonebook() {
+
+        String expected = "123 4567";
+        PhoneBook ph = new PhoneBook();
+        ph.add("joe", expected);
+        String actual = ph.lookup("joe");
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testRemoveToPhonebook() {
+
+        String expected = "123 4567";
+        PhoneBook ph = new PhoneBook();
+        ph.add("joe", expected);
+        ph.remove("joe", expected);
+        String actual = ph.lookup("joe");
+        assertEquals(null,actual);
+    }
+
+    @Test
+    public void testListNamesToPhonebook() {
+
+        String listOfNames = "blaine\njoe\nmary\n";
+        PhoneBook ph = new PhoneBook();
+        ph.add("joe", "555 1212");
+        ph.add("mary", "555 1313");
+        ph.add("blaine", "555 1414");
+        String actual = ph.listNames();
+        //System.out.println("listnames \n"+actual);
+        assertEquals(listOfNames,actual);
+    }
+    @Test
+    public void testListNamesAndNumbersToPhonebook() {
+
+        String listOfNames = "blaine 555 1414\njoe 555 1212\nmary 555 1313\n";
+        PhoneBook ph = new PhoneBook();
+        ph.add("joe", "555 1212");
+        ph.add("mary", "555 1313");
+        ph.add("blaine", "555 1414");
+        String actual = ph.listNamesAndNumbers();
+        //System.out.println("listnames \n"+actual);
+        assertEquals(listOfNames,actual);
+    }
+
 }
